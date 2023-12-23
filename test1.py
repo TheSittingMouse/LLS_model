dcf0c1dc8ad53456cf8f4d3ba5098f81af39bc9f
TheSittingMouse <154677701+TheSittingMouse@users.noreply.github.com>
2023-12-23 16:12:24 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,39 +4,42 @@ import matplotlib.pyplot as plt
 import scipy.fft as fft
 import numpy as np
 
+# Initializing the market
 stocks_outstanding = 10000
 daily_int = 0.0001
 daily_div = 0.00015
 market = LSM.Market(daily_int, daily_div, stocks_outstanding, "BIST")
 market.set_dividend_increment(0.00015)
 
-# for i in range(50, 350, 30):
-    # market.add_agent(10, 1000, i, agent_std=0.02)
-
+# Adding the desired amount of agent to the market
 market.add_agent(25, 1000, 10, agent_std=0.03)
 market.add_agent(25, 1000, 26, agent_std=0.03)
 market.add_agent(25, 1000, 55, agent_std=0.03)
 market.add_agent(25, 1000, 120, agent_std=0.03)
 
-# market.add_agent(30, 1000, 120)
-
+# Setting the initial price and the initial history of the market
 init_hist = norm.rvs(loc=0.0001, scale=0.000125, size=400)
-# init_hist[-1] = 2.5
-# print("Init Hist:",init_hist, "\n")
 init_price = 4
 
+# Starting the simulation.
 results = market.simulate(0.3, init_hist, init_price, 5000, callback=True, inc_div=True)
+
+# Results can be saved if desired
 market.save_results(results, "four_agent_std_0_03_5000_")
+
+# Processing the results to show only the forward time values
 zero_index = int(np.where(results[0] == 0)[0])-1
 time = results[0][zero_index:]
 prices = results[1][zero_index:]
 volume = results[2][zero_index:]
 
+# Fourier transformation of the prices
 zero_pad_len = 1000*len(time)
 fft_prices = fft.fft(prices, zero_pad_len)/(zero_pad_len)
 fft_freq = fft.fftfreq(zero_pad_len)
 fft_interval = zero_pad_len//2
 
+# Plotting the price and the daily volume
 plt.figure()
 plt.subplot(211)
 plt.semilogy(time, prices)
@@ -47,9 +50,13 @@ plt.semilogy(time, volume)
 plt.ylabel("Volume")
 plt.xlabel("Time (days)")
 
+# Plotting the FFT results
 plt.figure()
 plt.plot(fft_freq[:fft_interval], abs(fft_prices)[:fft_interval])
+plt.xlabel("Frequency (1/days)")
+plt.ylabel("Amplitude")
 
+# Plotting the wealth distributions for inverstors with different memory lengths
 legend_names_1 = []
 plt.figure()
 for memory in market.wealth_dist:
@@ -65,39 +72,3 @@ for memory in frac_wealth:
     plt.plot(abs(frac_wealth[memory]))
 plt.legend(legend_names_2)
 plt.show()
-
-
-# prices = np.arange(0.1, 100, 1)
-# k = np.array([market.get_collective_demand(i, results[1][-1], results[3], len(results[3])) for i in prices])
-# plt.semilogy(prices, abs(k-market.num_stocks))
-# # plt.semilogy(prices, shares_os*np.ones(len(prices)))
-# plt.show()
-
-# l = market.get_eq_price(results[1][-1], results[3], len(results[3]))
-# for a in market.AgentsList:
-#     a.demand = a.get_demand(l[0], results[1][-1], 0.00001, 0.0001, results[3], len(results[3]))
-# print(l)
-# # print(results[3])
-# print("Demand:", np.sum([k.demand for k in market.AgentsList]))
-# print(market.AgentsList)
-
-
-
-###########################################################################################################
-
-
-
-# for agent in market.AgentsList:
-#     print(agent.money)
-
-# agent1 = LSM.Agent(1000, 10, "Bob")
-# prices = np.arange(0.1, 8, 0.1)
-# demand1 = [agent1.get_demand(k, 4, 0.003, 0.01, init_hist, (len(init_hist))) for k in prices]
-# demand2 = agent1.get_demand(100, 100, 0.003, 0.01, init_hist, len(init_hist))
-# print(agent1.stocks, agent1.demand)
-# plt.semilogy(prices, demand1)
-# plt.show()    
-
-
-
-# m2 = LSM.Market(0.01, 0.0001, )
